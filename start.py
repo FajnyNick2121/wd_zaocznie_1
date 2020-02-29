e7e9a93d3207e3120f8c8188437fe605e2505201
mwnuk <xmarcinwnukx@gmail.com>
2020-02-29 11:51:30 +0100

    master

---
+++ start.py
@@ -0,0 +1,21 @@
+print("Hello world!")
+
+# def main():
+#     for elem in kolekcja:
+#         costam()
+
+# lancuchy znakow 
+
+liczba = "ala"
+liczba = 0
+liczba = 0.01
+# PEP8
+imie = "ala"
+print(imie)
+print(type(imie))
+print(type(5))
+print(type(5.5))
+print(type(True))
+
+imie= str("Ala")
+liczba= str(100)
